3c38ba1c7fd1db3e05ed71bd4e4c028bc8797325
JhoJha <210865802+JhoJha@users.noreply.github.com>
2025-05-08 09:43:42 -0500

    agregue una funcion para sumar valores

--- prueba.py
+++ prueba.py
@@ -2,4 +2,9 @@ x = int(input("Ingrese el valor a evaluar : "))
 
 if x >= 0:
   print("El numero ingresado es mayor que 0")
-print("Gracias por participar mi king")
+print("Gracias por participar mi estimado")
+
+z =  float(input("Ingrese un valor"))
+f = float(input("Ingrese un valor"))
+
+print(f"la suma de ambos valores es:{z+f})
